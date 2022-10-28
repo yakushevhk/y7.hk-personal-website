38136c50d77bdbdd51a1430da74267a9090757d1
Hermann Yakushev <78789455+yakushevhk@users.noreply.github.com>
2022-10-28 23:38:21 +0300

    erf

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -9,6 +9,7 @@ import BodyClassName from 'react-body-classname'
 import useDarkMode from 'use-dark-mode'
 import { PageBlock } from 'notion-types'
 
+import { Tweet, Twitter } from 'react-static-tweets'
 
 // core notion renderer
 import { NotionRenderer, Code, Collection, CollectionRow } from 'react-notion-x'
@@ -159,5 +160,127 @@ export const NotionPage: React.FC<types.PageProps> = ({
     pageAside = <PageSocial />
   }
 
-  
+  return (
+    <Twitter.Provider
+      value={{
+        tweetAstMap: (recordMap as any).tweetAstMap || {},
+        swrOptions: {
+          fetcher: (id) =>
+            fetch(`/api/get-tweet-ast/${id}`).then((r) => r.json())
+        }
+      }}
+    >
+      <PageHead site={site} />
+
+      <Head>
+        <meta property='og:title' content={title} />
+        <meta property='og:site_name' content={site.name} />
+
+        <meta name='twitter:title' content={title} />
+        <meta property='twitter:domain' content={site.domain} />
+
+        {config.twitter && (
+          <meta name='twitter:creator' content={`@${config.twitter}`} />
+        )}
+
+        {socialDescription && (
+          <>
+            <meta name='description' content={socialDescription} />
+            <meta property='og:description' content={socialDescription} />
+            <meta name='twitter:description' content={socialDescription} />
+          </>
+        )}
+
+        {socialImage ? (
+          <>
+            <meta name='twitter:card' content='summary_large_image' />
+            <meta name='twitter:image' content={socialImage} />
+            <meta property='og:image' content={socialImage} />
+          </>
+        ) : (
+          <meta name='twitter:card' content='summary' />
+        )}
+
+        {canonicalPageUrl && (
+          <>
+            <link rel='canonical' href={canonicalPageUrl} />
+            <meta property='og:url' content={canonicalPageUrl} />
+            <meta property='twitter:url' content={canonicalPageUrl} />
+          </>
+        )}
+
+        <title>{title}</title>
+      </Head>
+
+      <CustomFont site={site} />
+
+      {isLiteMode && <BodyClassName className='notion-lite' />}
+
+      <NotionRenderer
+        bodyClassName={cs(
+          styles.notion,
+          pageId === site.rootNotionPageId && 'index-page'
+        )}
+        components={{
+          pageLink: ({
+            href,
+            as,
+            passHref,
+            prefetch,
+            replace,
+            scroll,
+            shallow,
+            locale,
+            ...props
+          }) => (
+            <Link
+              href={href}
+              as={as}
+              passHref={passHref}
+              prefetch={prefetch}
+              replace={replace}
+              scroll={scroll}
+              shallow={shallow}
+              locale={locale}
+            >
+              <a {...props} />
+            </Link>
+          ),
+          code: Code,
+          collection: Collection,
+          collectionRow: CollectionRow,
+          tweet: Tweet,
+          modal: Modal,
+          pdf: Pdf,
+          equation: Equation
+        }}
+        recordMap={recordMap}
+        rootPageId={site.rootNotionPageId}
+        fullPage={!isLiteMode}
+        darkMode={darkMode.value}
+        previewImages={site.previewImages !== false}
+        showCollectionViewDropdown={false}
+        showTableOfContents={showTableOfContents}
+        minTableOfContentsItems={minTableOfContentsItems}
+        defaultPageIcon={config.defaultPageIcon}
+        defaultPageCover={config.defaultPageCover}
+        defaultPageCoverPosition={config.defaultPageCoverPosition}
+        mapPageUrl={siteMapPageUrl}
+        mapImageUrl={mapNotionImageUrl}
+        searchNotion={searchNotion}
+        pageFooter={comments}
+        pageAside={pageAside}
+        footer={
+          <Footer
+            isDarkMode={darkMode.value}
+            toggleDarkMode={darkMode.toggle}
+          />
+        }
+      />
+
+
+
+      <CustomHtml site={site} />
+    </Twitter.Provider>
+  )
 }
